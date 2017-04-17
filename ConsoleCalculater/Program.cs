82a8f1bd6e15eecb564d14c04c2ba6a933d668c4
Igor Ganov <igor_ganov@live.ru>
2017-04-17 09:35:31 +0300

    clean

--- ConsoleCalculater/Program.cs
+++ ConsoleCalculater/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AbstractTest
 {
@@ -12,18 +8,11 @@ namespace AbstractTest
     {
         static void Main(string[] args)
         {
-            var s = "5*(x-2)+3";
-            var charLst = new[] { '*', '/', '+', '-', '(', ')' };
-            var valueList = new List<string>();
-            var operationList = new List<char>();
-
-            var str = "-sin(-3log(- 3 sin -1.5157 x ^ 3.5x / -x ^ 25.27 log-x)(18x + 3.87)/x^-20)";//"- 1 * - sin - 4 ^ - 2";
+            var str = "-sin(-3log(- 3 sin -1.5157 x ^ 3.5x / -x ^ 25.27 log-x)(18x + 3.87)/x^-20)";
             double dbX = -0.395;
-            Console.WriteLine($"x = {dbX}\n{str} = {Calc.Start(str, dbX)}");
             try
             {
-                //Console.WriteLine(new Add(new Umn(new Const(5), new Min(new Del(new Variable(), new Const(0)), new Const(2))), new Const(3)).SetIntValue(5));
-
+                Console.WriteLine($"x = {dbX}\n{str} = {Calc.Start(str, dbX)}");
             }
             catch (Exception e)
             {
@@ -40,19 +29,13 @@ namespace AbstractTest
         private static readonly char UnoMinus = '-';
         private static readonly char OpenBracket = '(';
         private static readonly char CloseBracket = ')';
-
         private static readonly char CharVariable = 'x';
-        private static readonly char[] CharList = { '*', '/', '+', '-', '^', '(', ')' };
-        private static readonly char[] CharGroup0 = { '+', '-' };
-        private static readonly char[] CharGroup1 = { '*', '/' };
-        private static readonly char[] CharGroup2 = { '^' };
-        private static readonly string[] StrGroup = { "sin", "cos", "log"};
-        private enum MyEnum {nun, sin, cos, log, min};
+        private enum FuncEnum {Nun, Sin, Cos, Log, Min};
 
 
         public static double Start(string startValue, double xValue)
         {
-            var node = (new LinkedList<char>(startValue.Where(ch => ch != ' '))).First;
+            var node = new LinkedList<char>(startValue.Where(ch => ch != ' ')).First;
             return StrResult(default(char), ref node, out char value).SetIntValue(xValue);
         }
 
@@ -83,7 +66,7 @@ namespace AbstractTest
                 {
                     if (operandPreor == 1)
                     {
-                        a = DuoOperand(a, DuoOperand(b, StrResult(operand, ref sValue, out char tempOperand), nextOperand), operand); // TODO break???
+                        a = DuoOperand(a, DuoOperand(b, StrResult(operand, ref sValue, out char tempOperand), nextOperand), operand);
                         operand = tempOperand;
                     }
                     else
@@ -140,7 +123,7 @@ namespace AbstractTest
                 blUno1 = true;
                 charter = charter.Next;
             }
-            FindFunc(ref charter, out MyEnum funcValue);
+            FindFunc(ref charter, out FuncEnum funcValue);
             if (charter.Value == OpenBracket)
             {
                 charter = charter.Next;
@@ -162,8 +145,7 @@ namespace AbstractTest
                 }
                 if (charter?.Value == CharVariable)
                 {
-                    aValue = new Umn(new Const(strNumbs == "" ? 1 : Double.Parse(strNumbs.Replace('.', ','))),
-                        new Variable());
+                    aValue = strNumbs == "" ? (IOperand) new Variable() : new Umn(new Const(Double.Parse(strNumbs.Replace('.', ','))), new Variable());
                     charter = charter.Next;
                 }
                 else
@@ -171,17 +153,16 @@ namespace AbstractTest
                     if (strNumbs == "") throw new Exception("Некорректная запись");
                     aValue = new Const(strNumbs == "" ? 1 : Double.Parse(strNumbs.Replace('.', ',')));
                 }
-                if (blUno2) aValue = UnoOperand(aValue, MyEnum.min);
+                if (blUno2) aValue = UnoOperand(aValue, FuncEnum.Min);
             }
             aValue = UnoOperand(aValue, funcValue);
-            if (blUno1) aValue = UnoOperand(aValue, MyEnum.min);
-            var p = aValue.SetIntValue(-0.395);
+            if (blUno1) aValue = UnoOperand(aValue, FuncEnum.Min);
             return aValue;
         }
 
-        private static void FindFunc(ref LinkedListNode<char> nodeValue, out MyEnum funcValue)
+        private static void FindFunc(ref LinkedListNode<char> nodeValue, out FuncEnum funcValue)
         {
-            funcValue = MyEnum.nun;
+            funcValue = FuncEnum.Nun;
             var strFunc = "";
             while(nodeValue != null && !NumbersPredicate(nodeValue.Value) && nodeValue.Value != UnoMinus && nodeValue.Value != OpenBracket && nodeValue.Value != CharVariable)
             {
@@ -193,35 +174,27 @@ namespace AbstractTest
             {
                 case "sin":
                 {
-                    funcValue = MyEnum.sin;
+                    funcValue = FuncEnum.Sin;
                     break;
                 }
                 case "cos":
                 {
-                    funcValue = MyEnum.cos;
+                    funcValue = FuncEnum.Cos;
                     break;
                 }
                 case "log":
                 {
-                    funcValue = MyEnum.log;
+                    funcValue = FuncEnum.Log;
                     break;
                 }
                 default:
                 {
-                    if(strFunc.Length > 0) throw new Exception("Неверный ввод");
+                    if(strFunc.Length > 0) throw new Exception("Некорректная запись");
                     break;
                 }
             }
         }
 
-        //static bool OperandCompare(string op, string nextOp)
-        //{
-        //    int opNum = CharGroup0.Any(ch => ch == Convert.ToString(op)) ? 0
-        //        : CharGroup1.Any(ch => ch == Convert.ToString(op)) ? 1 : 2; //TODO throw
-        //    int nextOpNum = CharGroup0.Any(ch => ch == Convert.ToString(nextOp)) ? 0
-        //        : CharGroup1.Any(ch => ch == Convert.ToString(nextOp)) ? 1 : 2;
-        //    return opNum >= nextOpNum;
-        //}
 
         static IOperand DuoOperand(IOperand a, IOperand b, char operand)
         {
@@ -244,46 +217,38 @@ namespace AbstractTest
                     value = new Up(a, b);
                     break;
                 default:
-                    throw new Exception("Неверная запись");
+                    throw new Exception("Некорректная запись");
             }
             return value;
         }
 
-        static IOperand UnoOperand(IOperand a, MyEnum operand)
+        static IOperand UnoOperand(IOperand a, FuncEnum operand)
         {
             IOperand value;
             switch (operand)
             {
-                case MyEnum.nun:
+                case FuncEnum.Nun:
                     value = a;
                     break;
-                case MyEnum.sin:
+                case FuncEnum.Sin:
                     value = new Sin(a);
                     break;
-                case MyEnum.cos:
+                case FuncEnum.Cos:
                     value = new Cos(a);
                     break;
-                case MyEnum.log:
+                case FuncEnum.Log:
                     value = new Ln(a);
                     break;
-                case MyEnum.min:
+                case FuncEnum.Min:
                     value = new UnoMin(a);
                     break;
                 default:
-                    throw new Exception("Неверная запись");
+                    throw new Exception("Некорректная запись");
             }
             return value;
         }
 
         static bool NumbersPredicate(char ch) => (ch >= '0') && (ch <= '9') || (ch == '.');
-
-        static string EnumToString(IEnumerable<char> enumChars)
-        {
-            var s = "";
-            foreach (var enumChar in enumChars)
-                s += enumChar != '.' ? enumChar : ',';
-            return s;
-        }
     }
 
     interface IOperand
@@ -303,8 +268,6 @@ namespace AbstractTest
 
         public double SetIntValue(double value)
         {
-            //intValue = value;
-            //throw new NotImplementedException();
             return _doubValue;
         }
     }
@@ -317,7 +280,6 @@ namespace AbstractTest
         {
             _doubValue = value;
             return _doubValue;
-            //throw new NotImplementedException();
         }
     }
 
@@ -349,7 +311,6 @@ namespace AbstractTest
         protected override double Operation(double a)
         {
             return Math.Sin(a);
-            //throw new NotImplementedException();
         }
     }
 
@@ -362,7 +323,6 @@ namespace AbstractTest
         protected override double Operation(double a)
         {
             return Math.Cos(a);
-            //throw new NotImplementedException();
         }
     }
 
@@ -375,7 +335,6 @@ namespace AbstractTest
         protected override double Operation(double a)
         {
             return Math.Log(a);
-            //throw new NotImplementedException();
         }
     }
 
@@ -388,7 +347,6 @@ namespace AbstractTest
         protected override double Operation(double a)
         {
             return -a;
-            //throw new NotImplementedException();
         }
     }
 
@@ -398,23 +356,18 @@ namespace AbstractTest
         private readonly IOperand _value2;
         private double _resultValue;
 
-        //private int varible;
         protected Method(IOperand a, IOperand b)
         {
             _value1 = a;
             _value2 = b;
-            //Operation(a, b);
         }
 
         public abstract double Operation(double a, double b);
 
         public double SetIntValue(double value)
         {
-
             _resultValue = Operation(_value1.SetIntValue(value), _value2.SetIntValue(value));
             return _resultValue;
-            //_value.SetIntValue(value);
-            //throw new NotImplementedException();
         }
     }
 
@@ -427,7 +380,6 @@ namespace AbstractTest
         public override double Operation(double a, double b)
         {
             return (a - b);
-            //throw new NotImplementedException();
         }
     }
 
@@ -440,7 +392,6 @@ namespace AbstractTest
         public override double Operation(double a, double b)
         {
             return a + b;
-            //throw new NotImplementedException();
         }
     }
 
@@ -453,7 +404,6 @@ namespace AbstractTest
         public override double Operation(double a, double b)
         {
             return a * b;
-            //throw new NotImplementedException();
         }
     }
 
@@ -467,7 +417,6 @@ namespace AbstractTest
         {
             if (Math.Abs(b) < 0.000000000000000000000001) throw new Exception("не делите на ноль");
             return a / b;
-            //throw new NotImplementedException();
         }
     }
     class Up : Method
@@ -478,10 +427,7 @@ namespace AbstractTest
 
         public override double Operation(double a, double b)
         {
-            //if (Math.Abs(b) % 2 < 0.000000001 && a < 0) throw new Exception("отрицательный показатель");
-            var c = Math.Pow(a, b);
             return Math.Pow(a, b);
-            //throw new NotImplementedException();
         }
     }
 
